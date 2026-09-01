1bc918b121fe349243cc2c0cb38aab0ddc8e65c5
Gabu <97574076+Gabuwuw0210@users.noreply.github.com>
2026-09-01 00:28:54 -0400

    Refactor file handling in main.go
    Refactor file path constants to variables and add configuration function.

--- servidor/main.go
+++ servidor/main.go
@@ -1,12 +1,37 @@
 package main
 
-const (
-	archivoUsuarios  = "usuarios.csv"
-	archivoSesiones  = "sesiones.csv"
-	archivoHistorial = "historial.csv"
+import (
+	"os"
+	"path/filepath"
 )
 
+var (
+	archivoUsuarios  string
+	archivoSesiones  string
+	archivoHistorial string
+)
+
+func configurarArchivos() {
+
+	directorio, err := os.Getwd()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if filepath.Base(directorio) == "servidor" {
+		directorio = filepath.Dir(directorio)
+	}
+
+	archivoUsuarios = filepath.Join(directorio, "usuarios.csv")
+	archivoSesiones = filepath.Join(directorio, "sesiones.csv")
+	archivoHistorial = filepath.Join(directorio, "historial.csv")
+}
+
 func main() {
+
+	configurarArchivos()
+
 	iniciarServidorHTTP()
 	iniciarServidorTCP()
 	iniciarServidorUDP()
